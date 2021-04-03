c48b3c4e5663eade1b2604ad51bd474c3eea52e5
Aidan McGuire <>
2021-04-03 10:38:03 -0600

    Add app show page, user story 2 complete

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,8 @@ class ApplicationsController < ApplicationController
   def index
     @applications = Application.all
   end
+
+  def show
+    @application = Application.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -38,4 +38,6 @@ Rails.application.routes.draw do
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
 
   get '/applications', to: 'applications#index'
+  get '/applications/:id', to: 'applications#show'
+  get '/applications/:id/pets/:id', to: 'pets#show'
 end
