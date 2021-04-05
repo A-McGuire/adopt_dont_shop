5fe1c8bb91ec711232938dc9aecebf6d2e612188
Aidan McGuire <>
2021-04-04 19:47:06 -0600

    Add better feature tests for applications show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -68,6 +68,7 @@ RSpec.describe "the Applications show page" do
     expect(page).to have_button("Search")
 
     fill_in "Search", with: "#{pet1.name}"
+    click_button "Search"
 
     expect(current_path).to eq("/applications/#{application1.id}")
     expect(page).to have_content("#{pet1.name}")
@@ -81,13 +82,42 @@ RSpec.describe "the Applications show page" do
     pet2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
     pet3 = Pet.create!(adoptable: true, age: 4, breed: 'Shiba', name: 'Broccoli', shelter_id: shelter.id)
     
-    PetApplication.create!(pet: pet1, application: application1)
-    PetApplication.create!(pet: pet2, application: application1)
-    PetApplication.create!(pet: pet3, application: application1)
-
     visit "/applications/#{application1.id}"
 
     expect(page).to_not have_content("Add a Pet to this Application")
     expect(page).to_not have_button("Search")
+  end
+
+  it 'allows me to add a pet to my application' do
+    application1 = Application.create!(name: "app1", street_address: "123 street", city: "Denver", state: "CO", zip_code: 80210, status: "In Progress", description: "I am good with dogs")
+    
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    pet2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+    pet3 = Pet.create!(adoptable: true, age: 4, breed: 'Shiba', name: 'Broccoli', shelter_id: shelter.id)
+    
+    visit "/applications/#{application1.id}"
+
+    fill_in "Search", with: "#{pet1.name}"
+    click_button "Search"
+    
+    expect(current_path).to eq("/applications/#{application1.id}")
+    expect(page).to have_content("#{pet1.name}")
+    
+    expect(page).to have_button("Adopt this Pet")
+    click_button "Adopt this Pet"
+    expect(page).to have_content("#{pet1.name}")
+    
+    fill_in "Search", with: "#{pet2.name}"
+    click_button "Search"
+    
+    expect(current_path).to eq("/applications/#{application1.id}")
+    expect(page).to have_content("#{pet2.name}")
+    
+    expect(page).to have_button("Adopt this Pet")
+    click_button "Adopt this Pet"
+    
+    expect(page).to have_content("#{pet1.name}")
+    expect(page).to have_content("#{pet2.name}")
   end
 end
