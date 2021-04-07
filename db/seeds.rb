ef9aa89bb17f8ede5ad15138d49a04b15bb6f079
Aidan McGuire <>
2021-04-06 18:30:58 -0600

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,21 @@ Shelter.destroy_all
 Pet.destroy_all
 
 application1 = Application.create!(name: "app1", street_address: "123 street", city: "Denver", state: "CO", zip_code: 80210, status: "In Progress", description: "I am good with dogs")
-application2 = Application.create!(name: "app2", street_address: "456 street", city: "Brooklyn", state: "NY", zip_code: 11111, status: "Accepted", description: "I am great with dogs!")
+application2 = Application.create!(name: "app2", street_address: "456 street", city: "Brooklyn", state: "NY", zip_code: 11111, status: "In Progress", description: "I am great with dogs!!")
+application3 = Application.create!(name: "app3", street_address: "789 street", city: "Wooville", state: "WY", zip_code: 11111, status: "In Progress", description: "I am great with dogs!!!")
+application4 = Application.create!(name: "app4", street_address: "101 street", city: "Hooville", state: "TX", zip_code: 11111, status: "In Progress", description: "I am great with dogs!!!!")
 
 shelter1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
 shelter2 = Shelter.create!(name: 'Denver shelter', city: 'Denver, CO', foster_program: false, rank: 10)
+shelter3 = Shelter.create(name: 'fancy shelter', city: 'fancy, CO', foster_program: false, rank: 10)
+shelter4 = Shelter.create(name: 'ABC animal shelter', city: 'Harlingen, NM', foster_program: false, rank: 5)
+shelter5 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+
 pet1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter1.id)
 pet2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter1.id)
 pet3 = Pet.create!(adoptable: true, age: 4, breed: 'Shiba', name: 'Broccoli', shelter_id: shelter2.id)
 
 application1.pets.push(pet1,pet2)
 application2.pets.push(pet1,pet3)
-
-@shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-@shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-@shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+application3.pets.push(pet1,pet3,pet2)
+application4.pets.push(pet1)
