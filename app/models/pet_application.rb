42e97970b007d3a4e395ab62db9753cab8910ff4
Aidan McGuire <>
2021-04-08 14:53:30 -0600

    Remove old find pet app method and tests

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,8 +1,4 @@
 class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
-
-  # def self.find_pet_application(pet, application)
-  #   self.find_by(pet: pet, application: application)
-  # end
 end
