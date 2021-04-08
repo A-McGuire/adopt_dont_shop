80ad6a118fb1c38851a66cb690ef410ccc74fc9f
Aidan McGuire <>
2021-04-07 20:00:49 -0600

    Test that approving a pet app does not approve for all apps

--- spec/features/admin/admin_applications_show_spec.rb
+++ spec/features/admin/admin_applications_show_spec.rb
@@ -62,4 +62,31 @@ RSpec.describe 'admin applications show page' do
       expect(page).to_not have_content("Application Status: Approved")
     end
   end
+
+  it 'does not approve all applications for a pet across all applications' do
+    application1 = Application.create!(name: "app1", street_address: "123 street", city: "Denver", state: "CO", zip_code: 80210, status: "In Progress", description: "I am good with dogs")
+    application2 = Application.create!(name: "app2", street_address: "456 street", city: "Denver", state: "CO", zip_code: 80240, status: "In Progress", description: "I am v good with dogs")
+  
+    shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    pet2 = Pet.create!(adoptable: true, age: 2, breed: 'collie', name: 'fido', shelter_id: shelter.id)
+    pet3 = Pet.create!(adoptable: true, age: 3, breed: 'lab', name: 'max', shelter_id: shelter.id)
+    
+    application1.pets.push(pet1)
+    application2.pets.push(pet1)
+
+    visit "/admin/applications/#{application1.id}"
+    within("#pet-#{pet1.id}") do
+      click_button("Approve")
+      expect(page).to have_content("Application Status: Approved")
+      expect(page).to_not have_button("Approve")
+    end
+
+    visit "/admin/applications/#{application2.id}"
+    save_and_open_page
+    within("#pet-#{pet1.id}") do
+      expect(page).to have_button("Approve")
+      expect(page).to_not have_content("Application Status: Approved")
+    end
+  end
 end
