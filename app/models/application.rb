ef86df7d397d14b8e182300200eec647e4f59043
Aidan McGuire <>
2021-04-08 14:39:55 -0600

    Create find_pet_application(pet) for application

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,12 @@ class Application < ApplicationRecord
   
   validates :name, :street_address, :city, :state, :description, :status, presence: true
   validates :zip_code, presence: true, numericality: true
+
+  def find_pet_application(pet)
+    self.pet_applications.find_by(pet: pet)
+  end
 end
+
+ #  def self.find_pet_application(pet, application)
+  #   self.find_by(pet: pet, application: application)
+  # end
--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -3,8 +3,6 @@ class PetApplication < ApplicationRecord
   belongs_to :application
 
   def self.find_pet_application(pet, application)
-    # self.where(pet_id: pet.id, application_id: application.id)
-    # self.where(pet: pet, application: application)
     self.find_by(pet: pet, application: application)
   end
 end
