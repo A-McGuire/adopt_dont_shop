b1f34e3317a72c610fcf46932cfe8f151a2038c2
Aidan McGuire <>
2021-04-08 18:26:51 -0600

    Fix adopt pet button issue on refresh

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,15 +6,22 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])  
     @pets = Pet.search(params[:search])
-    if params[:adopt].present?
-      @pets_to_adopt = @application.pets << Pet.find(params[:adopt])
-    end
+    # if params[:adopt].present?
+    #   @pets_to_adopt = @application.pets << Pet.find(params[:adopt])
+    # end
   end
 
   def new
   end
 
   def create
+    if params[:adopt].present?
+      application = Application.find(params[:id])
+      application.pets << Pet.find(params[:adopt])
+      redirect_to "/applications/#{application.id}"
+      return
+    end
+
     application = Application.new(application_params)
     if application.save
       redirect_to "/applications/#{application.id}"
